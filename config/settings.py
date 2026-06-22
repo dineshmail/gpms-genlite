b3b63d1014e3705a7816801187da8460a8e6258d
Dinesh K <d860848@gmail.com>
2026-06-23 00:27:37 +0530

    GPMS-C1: Added Department model

--- config/settings.py
+++ config/settings.py
@@ -31,9 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-     'accounts',
+    'accounts',
     'production',
     'dashboard',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
