d20bdb326f3d68f834dbb346273de64dab1d08d9
Dinesh K <d860848@gmail.com>
2026-06-23 01:06:27 +0530

    GPMS-C1: Created Manager Dashboard page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from production.models import (
+    Department,
+    Issue,
+    ProductionUpdate,
+    AssetStock
+)
+
+def home(request):
+    context = {
+        "department_count": Department.objects.count(),
+        "issue_count": Issue.objects.count(),
+        "production_count": ProductionUpdate.objects.count(),
+        "asset_count": AssetStock.objects.count(),
+    }
+
+    return render(request, "dashboard/home.html", context)
